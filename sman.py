b0f03bb8c184b28fff841a387c33173d6cf9e5ac
sulaeman <raajipic@gmail.com>
2024-10-21 14:35:41 +0100

    commit

--- sman.py
+++ sman.py
@@ -1,3 +1,10 @@
 print('Hello World!!!')
+
 for i in range(5):
     print(i)
+
+colors = ["red", "blue", "green", "yellow", "orange"]
+for color in colors:
+    print(color + " " + "paint")
+    
+ 
